6751a962c653f57705d028b813fb489f67c0ef3e
LTC0213 <ltc970213@gmail.com>
2020-10-14 22:36:09 +0800

    USART_printf

---
+++ USART/USART_KEY/README.md
@@ -0,0 +1,4 @@
+# SMT32_Hal_CUBEMX
+USART1 TX与RX 需要短接 内部传输数据
+
+PA9 与 PA10 短接
---
+++ USART/USART_printf/MDK-ARM/printf.c
@@ -0,0 +1,41 @@
+#include "stdio.h"
+#include "usart.h"
+
+#pragma import(__use_no_semihosting)
+
+
+//标准库需要的支持函数
+struct __FILE
+{
+	int handle;
+};
+FILE __stdout;
+//定义_sys_exit()以避免使用半主机模式
+void _sys_exit(int x)
+{
+	x = x;
+}
+/**
+  * 函数功能: 重定向c库函数printf到DEBUG_USARTx
+  * 输入参数: 无
+  * 返 回 值: 无
+  * 说    明：无
+  */
+int fputc(int ch, FILE *f)
+{
+  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0xffff);
+  return ch;
+}
+
+/**
+  * 函数功能: 重定向c库函数getchar,scanf到DEBUG_USARTx
+  * 输入参数: 无
+  * 返 回 值: 无
+  * 说    明：无
+  */
+int fgetc(FILE * f)
+{
+  uint8_t ch = 0;
+  HAL_UART_Receive(&huart1,&ch, 1, 0xffff);
+  return ch;
+}
